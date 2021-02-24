09870ab5aec0e65ddcaedf95e6b7a66230cb059a
Brandon Gatlin <brandongatlin1981@me.com>
2021-02-24 00:06:30 -0600

    added hrefs to footer icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,25 @@
 import React from "react";
 import gitHub from "../images/github2.jpg";
 import linkedIn from "../images/linkedin.png";
-const strings = require('../strings.json');
+const strings = require("../strings.json");
 
 const Footer = ({ language, theme, classNameHandler }) => {
-    const formatCopyrightDate = (msg)=> {
-        return msg.replace('[START]', 2021).replace('[CURRENT]', new Date().getFullYear());
-    }
+  const formatCopyrightDate = (msg) => {
+    return msg
+      .replace("[START]", 2021)
+      .replace("[CURRENT]", new Date().getFullYear());
+  };
   return (
     <footer className={classNameHandler(theme, "footer") + " footer-grid"}>
-      <img className="footer-img gh" src={gitHub} />
-      <p className='text-center'>{formatCopyrightDate(strings[language].copyright)}</p>
-      <img className="footer-img li" src={linkedIn} />
+      <a className='gh' href="https://github.com/brandongatlin" target='_blank'>
+        <img className="footer-img" src={gitHub} />
+      </a>
+      <p className="text-center">
+        {formatCopyrightDate(strings[language].copyright)}
+      </p>
+      <a className='li' href="https://www.linkedin.com/in/brandon-gatlin/" target='_blank'>
+        <img className="footer-img" src={linkedIn} />
+      </a>
     </footer>
   );
 };
