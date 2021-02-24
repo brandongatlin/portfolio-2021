84c43e85bb88d772aec10c77f2f80f8e5646c5ac
Brandon Gatlin <brandongatlin1981@me.com>
2021-02-23 23:11:05 -0600

    more styles and added spanish footer text

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,16 @@
 import React from "react";
-import gitHub from "../images/github.png";
+import gitHub from "../images/github2.jpg";
 import linkedIn from "../images/linkedin.png";
+const strings = require('../strings.json');
 
 const Footer = ({ language, theme, classNameHandler }) => {
+    const formatCopyrightDate = (msg)=> {
+        return msg.replace('[START]', 2021).replace('[CURRENT]', new Date().getFullYear());
+    }
   return (
-    <footer className={classNameHandler(theme, "nav") + " footer-grid"}>
+    <footer className={classNameHandler(theme, "footer") + " footer-grid"}>
       <img className="footer-img gh" src={gitHub} />
-      <p>Copyright 2021</p>
+      <p className='text-center'>{formatCopyrightDate(strings[language].copyright)}</p>
       <img className="footer-img li" src={linkedIn} />
     </footer>
   );
